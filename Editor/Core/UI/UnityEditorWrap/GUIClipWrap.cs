3b5f421815c40e783dfe6c1997c0e42843415f7f
zengdelang <1147250901@qq.com>
2018-02-25 13:07:01 +0800

    update

--- Editor/Core/UI/UnityEditorWrap/GUIClipWrap.cs
+++ Editor/Core/UI/UnityEditorWrap/GUIClipWrap.cs
@@ -16,6 +16,9 @@ namespace EUTK
 
         protected static PushDelegate m_PushDelegate;
 
+        protected static Action<Rect, Vector2, Vector2, bool> s_PushAction;
+        protected static Action s_PopAction;
+
         public static Vector2 Unclip(Vector2 pos)
         {
             if (s_UnclipRectDelegate != null)
@@ -54,23 +57,46 @@ namespace EUTK
             }
         }
 
-        //优化
         public static void Push(Rect screenRect, Vector2 scrollOffset, Vector2 renderOffset, bool resetOffset)
         {
+            if (s_PushAction != null)
+            {
+                s_PushAction(screenRect, scrollOffset, renderOffset, resetOffset);
+                return;
+            }
+
             Type type = typeof(GUIUtility);
             type = type.Assembly.GetType("UnityEngine.GUIClip");
             var mf = type.GetMethod("Push",
                 BindingFlags.Static | BindingFlags.NonPublic, null, new Type[] { typeof(Rect), typeof(Vector2), typeof(Vector2), typeof(bool) }, null);
             mf.Invoke(null, new object[] { screenRect, scrollOffset, renderOffset, resetOffset });
+
+            var action = (Action<Rect, Vector2, Vector2, bool>)Delegate.CreateDelegate(typeof(Action<Rect, Vector2, Vector2, bool>), mf);
+            if (action == null)
+                throw new NullReferenceException("action");
+            s_PushAction = action;
+            s_PushAction(screenRect, scrollOffset, renderOffset, resetOffset);
         }
 
         public static void Pop()
         {
+            if (s_PopAction != null)
+            {
+                s_PopAction();
+                return;
+            }
+
             Type type = typeof(GUIUtility);
             type = type.Assembly.GetType("UnityEngine.GUIClip");
             var mf = type.GetMethod("Pop",
                 BindingFlags.Static | BindingFlags.NonPublic, null, new Type[] { }, null);
             mf.Invoke(null, new object[] { });
+
+            var action = (Action)Delegate.CreateDelegate(typeof(Action), mf);
+            if (action == null)
+                throw new NullReferenceException("action");
+            s_PopAction = action;
+            s_PopAction();
         }
     }
 }
